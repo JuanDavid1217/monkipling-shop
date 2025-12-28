089736a381c623a8246612fdf06d9fae28b7845a
Juand David <juandaviddelgadomunoz526@gmail.com>
2025-12-27 22:55:58 -0600

    Full-screen feature for the Gallery component fixed.

--- src/components/GalleryComponent.vue
+++ src/components/GalleryComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="content">
         <div class="principal-image">
-            <img :src="images[activeImage]" :class="{changing: isChanging}" @click="openFullScreen">
+            <img :src="isChanging?images[beforeImage]:images[activeImage]" :class="{changing: isChanging}" @click="openFullScreen">
             <div class="backward" @click="backward"><svg class="swiper-navigation-icon" width="11" height="20" viewBox="0 0 11 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.38296 20.0762C0.111788 19.805 0.111788 19.3654 0.38296 19.0942L9.19758 10.2796L0.38296 1.46497C0.111788 1.19379 0.111788 0.754138 0.38296 0.482966C0.654131 0.211794 1.09379 0.211794 1.36496 0.482966L10.4341 9.55214C10.8359 9.9539 10.8359 10.6053 10.4341 11.007L1.36496 20.0762C1.09379 20.3474 0.654131 20.3474 0.38296 20.0762Z" fill="currentColor"></path></svg></div>
             <div class="forward" @click="forward"><svg class="swiper-navigation-icon" width="11" height="20" viewBox="0 0 11 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.38296 20.0762C0.111788 19.805 0.111788 19.3654 0.38296 19.0942L9.19758 10.2796L0.38296 1.46497C0.111788 1.19379 0.111788 0.754138 0.38296 0.482966C0.654131 0.211794 1.09379 0.211794 1.36496 0.482966L10.4341 9.55214C10.8359 9.9539 10.8359 10.6053 10.4341 11.007L1.36496 20.0762C1.09379 20.3474 0.654131 20.3474 0.38296 20.0762Z" fill="currentColor"></path></svg></div>
             <div class="expand" @click="openFullScreen"><i class="bi bi-arrows-angle-expand"></i></div>
@@ -12,7 +12,7 @@
     </div>
     <div :class="['full-screen', {'open-full-screen': isFull}]">
         <div class="full-screen-wrapper">
-            <img :src="images[activeImage]" :class="{changing: isChanging}">
+            <img :src="isChanging?images[beforeImage]:images[activeImage]" :class="{changing: isChanging}">
             <div class="backward" @click="backward"><svg class="swiper-navigation-icon" width="11" height="20" viewBox="0 0 11 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.38296 20.0762C0.111788 19.805 0.111788 19.3654 0.38296 19.0942L9.19758 10.2796L0.38296 1.46497C0.111788 1.19379 0.111788 0.754138 0.38296 0.482966C0.654131 0.211794 1.09379 0.211794 1.36496 0.482966L10.4341 9.55214C10.8359 9.9539 10.8359 10.6053 10.4341 11.007L1.36496 20.0762C1.09379 20.3474 0.654131 20.3474 0.38296 20.0762Z" fill="currentColor"></path></svg></div>
             <div class="forward" @click="forward"><svg class="swiper-navigation-icon" width="11" height="20" viewBox="0 0 11 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.38296 20.0762C0.111788 19.805 0.111788 19.3654 0.38296 19.0942L9.19758 10.2796L0.38296 1.46497C0.111788 1.19379 0.111788 0.754138 0.38296 0.482966C0.654131 0.211794 1.09379 0.211794 1.36496 0.482966L10.4341 9.55214C10.8359 9.9539 10.8359 10.6053 10.4341 11.007L1.36496 20.0762C1.09379 20.3474 0.654131 20.3474 0.38296 20.0762Z" fill="currentColor"></path></svg></div>
             <div title="Cerrar" class="close-full-screen" @click="closeFullScreen"><i class="bi bi-x"></i></div>
@@ -256,6 +256,7 @@
         data() {
             return {
                 activeImage: 0,
+                beforeImage: 0,
                 isChanging: false,
                 galleryImages: [],
                 isFull: false,
@@ -277,6 +278,7 @@
                 
                 if (index != this.activeImage) {
                     this.isChanging = true;
+                    this.beforeImage = this.activeImage;
                     this.activeImage = index;
                     setTimeout (()=>{
                         this.isChanging = false;
@@ -287,6 +289,7 @@
                 if (this.isChanging) return;
 
                 this.isChanging = true;
+                this.beforeImage = this.activeImage;
                 if (this.activeImage == this.images.length-1) {
                     this.activeImage = 0;
                 }else{
@@ -303,6 +306,7 @@
                 if (this.isChanging) return;
 
                 this.isChanging = true;
+                this.beforeImage = this.activeImage;
                 if (this.activeImage == 0) {
                     this.activeImage = this.images.length-1;
                 }else{
